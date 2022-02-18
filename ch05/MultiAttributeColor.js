c91dcd48c274eea80876d24be2aeb1c7e65bb3b9
youxingzhi <xingzhi.you@shopee.com>
2022-02-18 10:16:18 +0800

    update

--- ch05/MultiAttributeColor.js
+++ ch05/MultiAttributeColor.js
@@ -7,91 +7,91 @@ var VSHADER_SOURCE =
   'void main() {\n' +
   '  gl_Position = a_Position;\n' +
   '  gl_PointSize = 10.0;\n' +
-  '  v_Color = a_Color;\n' +  // Pass the data to the fragment shader
-  '}\n';
+  '  v_Color = a_Color;\n' + // Pass the data to the fragment shader
+  '}\n'
 
 // Fragment shader program
 var FSHADER_SOURCE =
-  '#ifdef GL_ES\n' +
+  // '#ifdef GL_ES\n' +
   'precision mediump float;\n' + // Precision qualifier (See Chapter 6)
-  '#endif GL_ES\n' +
-  'varying vec4 v_Color;\n' +    // Receive the data from the vertex shader
+  // '#endif GL_ES\n' +
+  'varying vec4 v_Color;\n' + // Receive the data from the vertex shader
+  'uniform vec4 u_FragColor;\n' + // uniform変数
   'void main() {\n' +
-  '  gl_FragColor = v_Color;\n' +
-  '}\n';
+  '  gl_FragColor = u_FragColor;\n' +
+  '}\n'
 
 function main() {
   // Retrieve <canvas> element
-  var canvas = document.getElementById('webgl');
+  var canvas = document.getElementById('webgl')
 
   // Get the rendering context for WebGL
-  var gl = getWebGLContext(canvas);
+  var gl = getWebGLContext(canvas)
   if (!gl) {
-    console.log('Failed to get the rendering context for WebGL');
-    return;
+    console.log('Failed to get the rendering context for WebGL')
+    return
   }
 
   // Initialize shaders
   if (!initShaders(gl, VSHADER_SOURCE, FSHADER_SOURCE)) {
-    console.log('Failed to intialize shaders.');
-    return;
+    console.log('Failed to intialize shaders.')
+    return
   }
 
-  // 
-  var n = initVertexBuffers(gl);
+  //
+  var n = initVertexBuffers(gl)
   if (n < 0) {
-    console.log('Failed to set the vertex information');
-    return;
+    console.log('Failed to set the vertex information')
+    return
   }
 
   // Specify the color for clearing <canvas>
-  gl.clearColor(0.0, 0.0, 0.0, 1.0);
+  gl.clearColor(0.0, 0.0, 0.0, 1.0)
 
   // Clear <canvas>
-  gl.clear(gl.COLOR_BUFFER_BIT);
+  gl.clear(gl.COLOR_BUFFER_BIT)
 
   // Draw three points
-  gl.drawArrays(gl.POINTS, 0, n);
+  gl.drawArrays(gl.POINTS, 0, n)
 }
 
 function initVertexBuffers(gl) {
   var verticesColors = new Float32Array([
     // Vertex coordinates and color
-     0.0,  0.5,  1.0,  0.0,  0.0, 
-    -0.5, -0.5,  0.0,  1.0,  0.0, 
-     0.5, -0.5,  0.0,  0.0,  1.0, 
-  ]);
-  var n = 3; // The number of vertices
+    0.0, 0.5, 1.0, 0.0, 0.0, -0.5, -0.5, 0.0, 1.0, 0.0, 0.5, -0.5, 0.0, 0.0,
+    1.0,
+  ])
+  var n = 3 // The number of vertices
 
   // Create a buffer object
-  var vertexColorBuffer = gl.createBuffer();  
+  var vertexColorBuffer = gl.createBuffer()
   if (!vertexColorBuffer) {
-    console.log('Failed to create the buffer object');
-    return false;
+    console.log('Failed to create the buffer object')
+    return false
   }
 
   // Write the vertex coordinates and colors to the buffer object
-  gl.bindBuffer(gl.ARRAY_BUFFER, vertexColorBuffer);
-  gl.bufferData(gl.ARRAY_BUFFER, verticesColors, gl.STATIC_DRAW);
+  gl.bindBuffer(gl.ARRAY_BUFFER, vertexColorBuffer)
+  gl.bufferData(gl.ARRAY_BUFFER, verticesColors, gl.STATIC_DRAW)
 
-  var FSIZE = verticesColors.BYTES_PER_ELEMENT;
+  var FSIZE = verticesColors.BYTES_PER_ELEMENT
   //Get the storage location of a_Position, assign and enable buffer
-  var a_Position = gl.getAttribLocation(gl.program, 'a_Position');
+  var a_Position = gl.getAttribLocation(gl.program, 'a_Position')
   if (a_Position < 0) {
-    console.log('Failed to get the storage location of a_Position');
-    return -1;
+    console.log('Failed to get the storage location of a_Position')
+    return -1
   }
-  gl.vertexAttribPointer(a_Position, 2, gl.FLOAT, false, FSIZE * 5, 0);
-  gl.enableVertexAttribArray(a_Position);  // Enable the assignment of the buffer object
+  gl.vertexAttribPointer(a_Position, 2, gl.FLOAT, false, FSIZE * 5, 0)
+  gl.enableVertexAttribArray(a_Position) // Enable the assignment of the buffer object
 
   // Get the storage location of a_Position, assign buffer and enable
-  var a_Color = gl.getAttribLocation(gl.program, 'a_Color');
-  if(a_Color < 0) {
-    console.log('Failed to get the storage location of a_Color');
-    return -1;
+  var a_Color = gl.getUniformLocation(gl.program, 'u_FragColor')
+  if (a_Color < 0) {
+    console.log('Failed to get the storage location of a_Color')
+    return -1
   }
-  gl.vertexAttribPointer(a_Color, 3, gl.FLOAT, false, FSIZE * 5, FSIZE * 2);
-  gl.enableVertexAttribArray(a_Color);  // Enable the assignment of the buffer object
+  gl.vertexAttribPointer(a_Color, 3, gl.FLOAT, false, FSIZE * 5, FSIZE * 2)
+  gl.enableVertexAttribArray(a_Color) // Enable the assignment of the buffer object
 
-  return n;
+  return n
 }
